3b26e07f397b75e011d2a7d5f22f25e5ff71c4b6
Reid Paape <wrpaape@gmail.com>
2015-06-27 13:21:08 -0500

    building site header and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   resources :users, :products, :cart_items, :purchases
 
+  get  'login', to: 'sessions#new',    as: 'login'
+  post 'login', to: 'sessions#create', as: 'create_session'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'welcome#index'
 end
